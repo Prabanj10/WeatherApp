eeacf09d5674f1f17c7ed13544baa48615335e5f
Prabanj <prabanjk@gmail.com>
2025-02-09 03:39:58 +0530

    Add redux-persist to store in localstorage

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,6 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore,combineReducers } from '@reduxjs/toolkit'
 import weatherReducer  from './weatherSlice'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+  key: 'root', 
+  storage,
+};
+
+const rootReducer = combineReducers({
+  weather: weatherReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 
 export const store = configureStore({
-  reducer: {weather:weatherReducer},
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 })
+
+
+
+export const persistor = persistStore(store);
